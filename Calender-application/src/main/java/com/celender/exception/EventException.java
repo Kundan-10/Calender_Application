c61bd434a26313631253a7a969c92d6cc84215fa
Kundan-10 <kundanwakudkar10@gmail.com>
2023-04-26 21:44:16 +0530

    exentexeption add in expetions

---
+++ Calender-application/src/main/java/com/celender/exception/EventException.java
@@ -0,0 +1,12 @@
+package com.celender.exception;
+
+public class EventException extends Exception{
+
+	public EventException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
