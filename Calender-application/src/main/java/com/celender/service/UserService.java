dead0e2a7078a7d2cbe2e1d64a1e8b71a1a06651
Kundan-10 <kundanwakudkar10@gmail.com>
2023-04-26 21:59:17 +0530

    userservice added in service

---
+++ Calender-application/src/main/java/com/celender/service/UserService.java
@@ -0,0 +1,22 @@
+package com.celender.service;
+
+import java.util.List;
+
+import com.calender.model.Event;
+import com.calender.model.User;
+import com.celender.dto.LoginDto;
+import com.celender.dto.UserDto;
+import com.celender.exception.EventException;
+import com.celender.exception.UserException;
+
+public interface UserService {
+	
+	public User registerUser(User user)throws UserException;
+	public User updateUser(UserDto user) throws UserException;
+	
+	public String  loginUser(LoginDto login);
+
+	public List<Event> getEvent(String type,String email)throws EventException,UserException;
+}
+
+
