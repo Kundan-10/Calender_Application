b49fcbdb7d62e0d1813462633e936f69b27dd448
Kundan-10 <kundanwakudkar10@gmail.com>
2023-04-26 21:52:55 +0530

    GlobalException added in exception

--- Calender-application/src/main/java/com/celender/exception/MyError.java
+++ Calender-application/src/main/java/com/celender/exception/MyError.java
@@ -1,5 +1,19 @@
 package com.celender.exception;
 
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MyError {
+	
+	private String massage;
+	private LocalDate date;
+	private String description;
+	
 
 }
